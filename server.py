85edbf3012436e5fa57b24ab243827af296d4259
EvgenySimakovich <evgeny.simakovich@gmail.com>
2023-07-07 16:27:50 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,48 @@
+from pyModbusTCP.server import ModbusServer
+from time import sleep
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+
+load_dotenv()
+
+IP_ADDRESS = os.getenv('IP_ADDRESS')
+# create an instance of Modbus Server
+servers = [ModbusServer(IP_ADDRESS, i, no_block=True) for i in range(10502, 10504)]
+
+
+def main():
+
+    try:
+        print('Start server...')
+        for server in servers:
+            print(f'Server ({server.port}) started')
+            server.start()
+
+        print('Modbus server is online')
+
+        val = 1
+        while True:
+            print(datetime.now())
+            for server in servers:
+                server.data_bank.set_input_registers(address=0,
+                                                     word_list=[val for i in range(50000)])
+            print(datetime.now())
+
+            if val == 100:
+                val = 0
+            val += 1
+
+            sleep(1)
+
+    except:
+        print('Shutdown server...')
+        for server in servers:
+            print(f'Server ({server.port}) stopped')
+            server.stop()
+        print('Modbus server is offline')
+
+
+if __name__ == '__main__':
+    main()
+
